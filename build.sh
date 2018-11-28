185d03318bfff827847af0d2d01acf46a7305ce5
Peter Cossey <peter@thislittleduck.com>
2018-11-28 21:44:34 +1100

    Updating build and deploy scripts.

--- build.sh
+++ build.sh
@@ -4,24 +4,32 @@ set -e
 PROJECT_DIR=$(cd `dirname $0` && pwd)
 HUGO_VERSION="v0.49"
 
+echo
+echo "  +====================+"
+echo "  +====WEB GOALS HQ====+"
+echo "  +====================+"
+echo
+
 # check that we have the right version of hugo (if not, exit)
 if [ -x "$(command -v hugo)" ]; then
-    echo 'Found Hugo, checking version...';
+    echo '  Found Hugo, checking version...'
+    echo
     VERSION=$(hugo version | grep -o "v[0-9]*.[0-9]*")
-    echo "Local version is $VERSION. Checking support..."
+    echo "  Local version is $VERSION. Checking support..."
+    echo
     if [ $VERSION == $HUGO_VERSION ]; then
-        echo "This script supports Hugo version $HUGO_VERSION. Let's go!"
+        echo "  SUCCESS. This script supports Hugo version $HUGO_VERSION. Let's go!"
         echo
 
         # Delete the public folder if it exists so we create a fresh build.
         if [ -d $PROJECT_DIR/public ]; then
-            echo "Removing previous build folder $PROJECT_DIR/public"
+            echo "  Removing previous build folder $PROJECT_DIR/public"
             rm -rf $PROJECT_DIR/public
         fi
 
         #echo build project
         echo
-        echo "Now running hugo command to build site..."
+        echo "  Now running hugo command to build site..."
         hugo -s $PROJECT_DIR
 
     else
@@ -29,6 +37,6 @@ if [ -x "$(command -v hugo)" ]; then
         exit 1
     fi
 else
-    echo 'Cannot find MkDocs, exiting';
-    exit 1;
+    echo 'Cannot find Hugo, exiting'
+    exit 1
 fi
