d39ff9d3404f4a0ab1da767b149e3a3935f78f69
Peter Cossey <peter@thislittleduck.com>
2018-11-28 16:23:24 +1100

    First cut of build script.

--- build.sh
+++ build.sh
@@ -4,13 +4,30 @@ set -e
 PROJECT_DIR=$(cd `dirname $0` && pwd)
 HUGO_VERSION="v0.49"
 
-echo $PROJECT_DIR
-
 # check that we have the right version of hugo (if not, exit)
 if [ -x "$(command -v hugo)" ]; then
     echo 'Found Hugo, checking version...';
     VERSION=$(hugo version | grep -o "v[0-9]*.[0-9]*")
-    echo 'Local hugo version is ' . $VERSION
+    echo "Local version is $VERSION. Checking support..."
+    if [ $VERSION == $HUGO_VERSION ]; then
+        echo "This script supports Hugo version $HUGO_VERSION. Let's go!"
+        echo
+
+        # Delete the public folder if it exists so we create a fresh build.
+        if [ -d $PROJECT_DIR/public ]; then
+            echo "Removing previous build folder $PROJECT_DIR/public"
+            rm -rf $PROJECT_DIR/public
+        fi
+
+        #echo build project
+        echo
+        echo "Now running hugo command to build site..."
+        hugo -s $PROJECT_DIR
+
+    else
+        echo "When we run the 'hugo version' it says $VERSION but this script only supports $HUGO_VERSION."
+        exit 1
+    fi
 else
     echo 'Cannot find MkDocs, exiting';
     exit 1;
